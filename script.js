4f003dac72b8a8d3bc1c1223c7ddb309a93114f8
Pankaj <pankajkushwaha251@gmail.com>
2023-04-26 20:31:46 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
-  // To check a number or not
-  function checkNumber(num){
-    let pattern = new RegExp("[0-9]{10}");
-    let bool = pattern.test(num);
-    // console.log(bool, "regular expression value");
-    return bool;
-    
-  }
-  // To check number length
-  function checkValidation(value){
-    value = value.trim();
-    if(value.length == 12){
-      if(value.includes("91")){
-        let num = value.substr(2);
-        if(checkNumber(num))
-          return num;
-      }
-    }
-    if(value.length == 13){
-      if(value.includes("+91")){
-        let num = value.substr(3);
-        if(checkNumber(num))
-          return num;
-      }
-    }
-    if(value.length == 10){
-      if(checkNumber(value))
-        return value;
-    }
+// To check only numbers
+function checkNumber(num){
+  let pattern = new RegExp("[0-9]{10}");
+  let bool = pattern.test(num);
+  return bool;
+}
+// To check number length and validation
+function checkValidation(value){
+  text = value.trim(); // remove extreme left and right space
+  value = text.replace(/\s/g, ""); // Remove the extra space in between number
+  let len = value.length;
+  if(len < 10){
+    return false;
+  }else if(len > 13){
+    return false;
+  }else if(len == 11){
     return false;
   }
 
-  // Add "Click" event listener to the submit button
-  const submit = document.getElementById('submit');
-  submit.addEventListener('click', (e)=> {
+  let condition = (len == 12) && (value.charAt(0) == "9" && value.charAt(1) == "1");
+  if(condition){
+      let num = value.substr(2);
+      if(checkNumber(num))
+        return num;
+  }
+  condition = (len == 13) && (value.charAt(0) == "+" && value.charAt(1) == "9" && value.charAt(2) == "1");
+  if(condition){
+      let num = value.substr(3);
+      if(checkNumber(num))
+        return num;
+  }
+  condition = len == 10;
+  if(condition){
+    if(checkNumber(value))
+      return value;
+  }
+  return false;
+}
 
-    const value = document.getElementById('inputBox').value;
-    const number = checkValidation(value);
-    // console.log(number, typeof number);
-    if(number){
-      location.href = `https://wa.me/91${number}`;
-    }else{
-      document.getElementById('alert').innerHTML = "Invalid number, Enter a valid number";
-    }
+// Add "Click" event listener to the submit button
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (e)=> {
+  const value = document.getElementById('inputBox').value;
+  const message = document.getElementById('inputText').value.trim();
+  const number = checkValidation(value);
+  console.log(number);
+  if(number){
+    // location.href = `https://wa.me/91${number}`;
+    location.href = `https://api.whatsapp.com/send?type=phone_number&app_absent=0&phone=91${number}&text=${message}`;
+  }else{
+    document.getElementById('alert').innerHTML = "Invalid number, Enter a valid number";
+  }
 
-  });
+});
  
